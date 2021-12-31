f9b63debe57dc0dcb1c65416c16604ed67d24bc2
jcm2512 <joe.magalotti@gmail.com>
2022-01-01 02:18:17 +0900

    sanity

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({data}) {
+  const recipes = data.recipes;
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +15,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <a href="">{recipes[0].title}</a>
         </h1>
 
         <p className={styles.description}>
@@ -66,4 +68,14 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export function getStaticProps() {
+  return {
+    props: {
+      data: {
+        recipes: [{title: "Pineapple Smoothie"}],
+      },
+    },
+  };
 }
